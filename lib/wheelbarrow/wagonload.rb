b58ae4d1824ddfb236caf01fd9a48fda109642a8
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2012-09-27 23:17:11 +0200

    Naive implementation of pull, migrate and precompile

--- lib/wheelbarrow/wagonload.rb
+++ lib/wheelbarrow/wagonload.rb
@@ -7,14 +7,26 @@ module Wheelbarrow
 
     # Git pull from repo to deploy dir
     def pull
+      Dir.chdir @deployment_dir do
+        `git pull #{@repo} master`
+        raise "Deployment of code failed" unless $?.exitstatus == 0
+      end
     end
 
     # rake db:migrate
     def run_migrations
+      Dir.chdir @deployment_dir do
+        puts `RAILS_ENV=production rake db:migrate`
+        raise "Migrating the database failed" unless $?.exitstatus == 0
+      end
     end
 
     # asset precompiling
     def assets_precompiling
+      Dir.chdir @deployment_dir do
+        puts `RAILS_ENV=production rake assets:precompile`
+        raise "Assets precompiling failed" unless $?.exitstatus == 0
+      end
     end
 
     # restart unicorn
